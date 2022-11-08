51f7f81ee94188affca638ee96474434fc8d1c03
evvolpe <69252238+evvolpe@users.noreply.github.com>
2022-11-08 15:45:35 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,16 @@ terraform {
     }
   }
 }
+
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.43.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+}
