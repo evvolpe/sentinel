58cc62ca6a99dfab44459c048512a4e6700cfbb3
evvolpe <69252238+evvolpe@users.noreply.github.com>
2022-11-21 11:34:20 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,11 +19,11 @@ terraform {
 
 provider "google" {
   project_id = "tf-sentinel-366917"
-  credentials = secrets.GCP_CREDENTIALS
+  credentials = var.GCP_CREDENTIALS
 }
 provider "google-beta" {
   project_id = "tf-sentinel-366917"
-  credentials = secrets.GCP_CREDENTIALS
+  credentials = var.GCP_CREDENTIALS
 }
 
 module "network_example_multi_vpc" {
